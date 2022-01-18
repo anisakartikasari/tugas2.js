11ec3428541f4c85e96fd6168f0bcedbbdc3cd61
anisakartikasari <03anisaks@gmail.com>
2022-01-17 23:22:50 -0800

    tugas2

---
+++ index.js
@@ -0,0 +1,11 @@
+function perkenalan(nama, kelas, jurusan){
+    console.log("Nama saya " + nama + " kelas " + kelas + " jurasan saya " + jurusan)
+}
+
+perkenalan ("Anisa", "XI", "Rekasa Perangkat Lunak")
+
+function novel(judul, pengarang, jmlhhalaman){
+    console.log("Judul buku " + judul + " pengarangnya " + pengarang + " jumlah halamannya " + jmlhhalaman)
+}
+
+novel ("Laut Bercerita", "Leila Salikha Chudori", 379)
