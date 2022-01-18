b770a23d64249e958d9a149f8edc59773649e16f
anisakartikasari <03anisaks@gmail.com>
2022-01-17 23:27:55 -0800

    tugas2

---
+++ soalno5.js
@@ -0,0 +1,11 @@
+function lessThanOrEqualToZero(a){
+    if (a < 0 || a==0) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }
+}
+
+lessThanOrEqualToZero (5);
+lessThanOrEqualToZero (0);
+lessThanOrEqualToZero (-2);
